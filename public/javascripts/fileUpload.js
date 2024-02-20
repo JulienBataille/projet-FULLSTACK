671cc4dee52fde8f3c5052e5f1524699c51c8add
landesprit <julien.bataille40@gmail.com>
2024-02-20 11:49:48 +0100

    Finished

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,29 @@
-FilePond.registerPlugin(
+const rooStyles = window.getComputedStyle(document.documentElement)
+
+if (rooStyles.getPropertyValue('--book-cover-width-large') != null && rooStyles.getPropertyValue('--book-cover-width-large') !==''){
+    ready()
+} else{
+    document.getElementById('main.css')
+    .addEventListener('load', ready)
+}
+
+
+function ready() {
+    const coverWidth = parseFloat( rooStyles.getPropertyValue('--book-cover-width-large'));
+    const coverAspectRatio = parseFloat( rooStyles.getPropertyValue('--book-cover-aspect-ratio'));
+    const coverHeight = coverWidth /coverAspectRatio;
+    FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
 )
 
 FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
+    stylePanelAspectRatio: 1/ coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight,
 })
 
 FilePond.parse(document.body);
+}
+
